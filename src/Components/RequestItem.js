d55c2b7fea2db7e738739e460d439ab63f52b890
Siddharth Roy <siddharthroy36912@gmail.com>
2021-09-30 16:19:56 +0530

    Added menu

--- src/Components/RequestItem.js
+++ src/Components/RequestItem.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
@@ -65,16 +65,58 @@ const DropdownBtn = styled.button`
 
 const DropdownMenu = styled.div`
 	position: absolute;
-	right: 0;
+	right: -10px;
 	top: 2rem;
-	padding: 1rem;
-	border: 1px solid red;
+	border: ${(props) => props.theme.borderStyle};
+	background-color: #2A2A2A;
 	z-index: 2;
+	padding: 0.2rem 0;
+	border-radius: 3px;
+	display: flex;
+	flex-direction: column;
+	width: 10rem;
+`
+
+const MenuDivider = styled.hr`
+	border: none;
+	border-bottom: 1px solid #3e3e3e;
+	margin: 0.2rem 0;
+`
+
+const MenuItem = styled.button`
+	border: none;
+	background-color: unset;
+	color: ${(props) => props.red ? '#F45866' : 'white'};
+	padding: 0.5rem 1rem;
+	width: 100%;
+	text-align: start;
+
+	> i {
+		margin-right: 1rem;
+	}
+
+	:hover {
+		background-color: rgba(225,225,225, 0.1);
+	}
 `
 
 export default function RequestItem({ id, requestId }) {
 	const [menuOpen, setMenuOpen] = useState(false)
+	const menuEl = useRef(null);
 	const requestInfo = useSelector(state => state.project[id].requests[requestId])
+
+	const handleClickOutside = (event) => {
+		if (menuEl.current && !menuEl.current.contains(event.target)) {
+			setMenuOpen(false)
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', handleClickOutside, true);
+    return () => {
+        document.removeEventListener('click', handleClickOutside, true);
+    };
+	})
 
 	return (
 		<Container>
@@ -88,8 +130,20 @@ export default function RequestItem({ id, requestId }) {
 				/>
 			</DropdownBtn>
 			{menuOpen && (
-				<DropdownMenu>
-
+				<DropdownMenu ref={menuEl}>
+					<MenuItem>
+						<i class="bi bi-files"></i>
+						Duplicate
+					</MenuItem>
+					<MenuItem>
+						<i class="bi bi-pin-angle"></i>
+						Pin
+					</MenuItem>
+					<MenuDivider />
+					<MenuItem red>
+						<i class="bi bi-file-earmark-x-fill"></i>
+						Delete
+					</MenuItem>
 				</DropdownMenu>
 			)}
 		</Container>
